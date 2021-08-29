2659ea55fa4f8dae5047b85521549527f679158b
qiaoshu <1748531436@qq.com>
2021-08-29 16:47:52 +0800

    角色查看表单验证

--- src/views/employees/detail/login-setting.vue
+++ src/views/employees/detail/login-setting.vue
@@ -4,16 +4,17 @@
     label-width="120px"
     style="margin-left: 120px; margin-top: 30px"
     :model="user"
+    :rules="rules"
   >
-    <el-form-item label="姓名:">
+    <el-form-item label="姓名:" prop="username">
       <el-input v-model="user.username" style="width:300px" />
     </el-form-item>
-    <el-form-item label="密码:">
+    <el-form-item label="密码:" prop="password">
       <el-input v-model="user.password" type="password" style="width:300px" />
     </el-form-item>
 
     <el-form-item>
-      <el-button type="primary" @click="$store.dispatch('user/putSysUser',user)">更新</el-button>
+      <el-button type="primary" @click="putSysUser">更新</el-button>
       <el-button @click="$router.back()">返回</el-button>
     </el-form-item>
   </el-form>
@@ -25,7 +26,14 @@ import { GetUserAPI, PutSysUserAPI } from '@/api'
 export default {
   data() {
     return {
-      user: {}
+      user: {},
+      rules: {
+        username: [
+          { required: true, message: '用户姓名不能为空', trigger: 'blur' },
+          { min: 1, max: 4, message: '用户姓名为1-4位', trigger: 'blur' }
+        ],
+        password: [{ required: true, message: '密码不能为空', triggler: 'blur' }]
+      }
     }
   },
   created() {
@@ -37,14 +45,20 @@ export default {
       this.user = res
     },
     // 保存员工基本信息
-    async putSysUser() {
-      try {
-        await PutSysUserAPI(this.user)
-        this.$message('修改成功')
-      } catch (err) {
-        console.error(err)
-        this.$message('修改失败')
-      }
+    putSysUser() {
+      this.$refs.userForm.validate(async value => {
+        if (value) {
+          try {
+            await PutSysUserAPI(this.user)
+            this.$message('修改成功')
+          } catch (err) {
+            console.error(err)
+            this.$message('修改失败')
+          }
+        } else {
+          this.$message('请填写信息')
+        }
+      })
     }
   }
 
--- src/views/employees/detail/user-info.vue
+++ src/views/employees/detail/user-info.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="user-info">
     <!-- 个人信息 -->
-    <el-form label-width="220px" :model="user">
+    <el-form ref="userForm" label-width="220px" :model="user" :rules="rules">
       <!--手机 -->
-      <el-form-item label="手机">
+      <el-form-item label="手机" prop="mobile">
         <el-input v-model="user.mobile" style="width:220px" />
       </el-form-item>
       <!-- 工号 入职时间 -->
-      <el-form-item label="入职时间">
+      <el-form-item label="入职时间" prop="timeOfEntry">
         <el-date-picker
           v-model="user.timeOfEntry"
           type="date"
@@ -23,7 +23,7 @@
 
       <!-- 保存个人信息 -->
       <el-form-item>
-        <el-button type="primary" @click="getUser">保存更新</el-button>
+        <el-button type="primary" @click="putSysUser">保存更新</el-button>
         <el-button @click="$router.back()">返回</el-button>
       </el-form-item>
     </el-form>
@@ -36,7 +36,17 @@ import { GetUserAPI, PutSysUserAPI } from '@/api'
 export default {
   data() {
     return {
-      user: {}
+      user: {},
+      rules: {
+        mobile: [
+          { required: true, message: '手机号不能为空', trigger: 'blur' },
+          { pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确', trigger: 'blur' }
+        ],
+        timeOfEntry: [
+          { required: true, message: '请选择入职时间', trigger: 'blur' }
+        ]
+      }
+
     }
   },
   created() {
@@ -48,13 +58,19 @@ export default {
       this.user = res
     }, // 保存员工基本信息
     async putSysUser() {
-      try {
-        await PutSysUserAPI(this.user)
-        this.$message('修改成功')
-      } catch (err) {
-        console.error(err)
-        this.$message('修改失败')
-      }
+      this.$refs.userForm.validate(async value => {
+        if (value) {
+          try {
+            await PutSysUserAPI(this.user)
+            this.$message('修改成功')
+          } catch (err) {
+            console.error(err)
+            this.$message('修改失败')
+          }
+        } else {
+          this.$message('请填写信息')
+        }
+      })
     }
 
   }
